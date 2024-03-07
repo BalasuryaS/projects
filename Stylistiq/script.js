450c09818c0ec33bbd665d1657413ab95b283735
BalasuryaS <98301831+BalasuryaS@users.noreply.github.com>
2024-03-07 07:28:46 +0530

    Add files via upload

---
+++ Stylistiq/script.js
@@ -0,0 +1,12 @@
+var sidenavbar1=document.querySelector(".side-navbar")
+
+function showbar1()
+{
+    sidenavbar1.style.left="0"
+
+}
+
+function closebar()
+{
+    sidenavbar1.style.left="-60%"
+}
